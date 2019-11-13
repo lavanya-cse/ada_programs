8da35191cfd396d28c1a93816228f0d99527db61
lavanya-cse <53851692+lavanya-cse@users.noreply.github.com>
2019-11-13 13:04:09 +0530

    Update pro 9_b.c

--- pro 9_b.c
+++ pro 9_b.c
@@ -1,23 +1,22 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int a,b,u,v,n,i,j,ne=1;
 int visited[10]={0},min,mincost=0,cost[10][10];
 
 int main()
 {
-	cout<<"Enter the number of nodes";
-	cin>>"%d",&n;
-	cout<<"Enter the adjacency matrix\n";
+	printf("Enter the number of nodes");
+	scanf("%d",&n);
+	printf("Enter the adjacency matrix\n");
 	for(i=1;i<=n;i++)
 		for(j=1;j<=n;j++)
 		{
-			cin>>cost[i][j];
+			scanf("%d",&cost[i][j]);
 			if(cost[i][j]==0)
 				cost[i][j]=999;
 		}
 		visited[1]=1;
-		cout<<"\n";
+		printf("\n");
 		while(ne<n)
 		{
 			for(i=1,min=999;i<=n;i++)
@@ -31,13 +30,12 @@ int main()
 						}
 						if(visited[a]==0 || visited[b]==0)
 						{
-							//cout<<"Edge %d:(%d %d)distance:%d\n",ne++,a,b,min;
-							cout<<"Edge"<<ne++,<<":("<<a<<b<<")distance"<<min;
+							printf("Edge %d:(%d %d)distance:%d\n",ne++,a,b,min);
 							mincost+=min;
 							visited[b]=1;
 							
 						}
 						cost[a][b]=cost[b][a]=999;
 		}
-		cout<<"Minimum distance:"<<mincost;
+		printf("Minimum distance=%d\n",mincost);
 }
